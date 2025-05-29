e741adf499101dd0e75b2abf91d18053ae62ba7d
Mohamed Abdul Kader <mak2503sde@gmail.com>
2025-05-29 19:58:45 +0530

    create hero

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='h-screen flex flex-col justify-center items-center bg-[#8F8F8F] space-y-4'>
+      <p>Performance in motion</p>
+      <h1 className='text-3xl font-bold'>Soft Trims and NVH Solutions</h1>
+      <p className='text-3xl font-normal'>for seamless rides</p>
+    </div>
+  )
+}
+
+export default Hero
